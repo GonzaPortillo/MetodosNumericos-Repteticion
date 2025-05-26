fc47476eb04df331a7a32db1fca04f5ee9360693
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-05-26 12:15:01 -0600

    Create Ejemplo_5.java

---
+++ Tema2/Regla Falsa/Ejemplo_5.java
@@ -0,0 +1,24 @@
+public class ReglaFalsa5 {
+    public static double f(double x) {
+        return Math.log(x) + x;
+    }
+
+    public static void main(String[] args) {
+        double a = 0.1, b = 1, tol = 1e-6;
+        int maxIter = 100;
+        double fa = f(a), fb = f(b), c = a;
+
+        for (int i = 0; i < maxIter; i++) {
+            c = b - fb * (b - a) / (fb - fa);
+            double fc = f(c);
+            if (Math.abs(fc) < tol) break;
+            if (fa * fc < 0) {
+                b = c; fb = fc;
+            } else {
+                a = c; fa = fc;
+            }
+        }
+
+        System.out.printf("Raíz aproximada: %.6f\n", c);
+    }
+}
