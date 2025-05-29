b99d6abb54aecc38388cb6f72d02d75608e93b04
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-05-29 10:04:04 -0600

    Update Ejemplo2.java

--- Tema1/Error Absoluto/Ejemplo2.java
+++ Tema1/Error Absoluto/Ejemplo2.java
@@ -1,4 +1,5 @@
 public class Ejemplo2{
+  
   public static void main(String []args){
     double valorreal = 75.2;
     double valoraprox = 73.5;
@@ -6,5 +7,7 @@ public class Ejemplo2{
     double errorab = Math.abs(valorreal - valoraprox);
 
     System.out.printf("Error Absoluto %.2f%n", errorab);
+
   }
+
 }
