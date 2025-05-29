40494b66977889ecda54651d2711db23018209c6
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-05-29 06:42:49 -0600

    Update Ejemplo1.java

--- Tema1/Error Absoluto/Ejemplo1.java
+++ Tema1/Error Absoluto/Ejemplo1.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
 
+public class Main {
+    public static ArrayList<Double> calcularErrorAbsoluto(double[][] valores) {
+        ArrayList<Double> errores = new ArrayList<>();
+        for (double[] par : valores) {
+            double vr = par[0];
+            double va = par[1];
+            errores.add(Math.abs(vr - va));
+        }
+        return errores;
+    }
+
+    public static void main(String[] args) {
+        double[][] datos = {
+            {120, 118},
+            {85, 83},
+            {95, 92}
+        };
+
+        System.out.println(calcularErrorAbsoluto(datos));
+    }
+}
