7c18f82e6e5ecbc610509acd350f3c8ba9eef9f5
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-05-29 06:37:47 -0600

    Update Ejemplo1.java

--- Tema1/Error Relativo/Ejemplo1.java
+++ Tema1/Error Relativo/Ejemplo1.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double valorReal = 100.0;
+        double valorAproximado = 98.5;
+
+        double errorRelativo = Math.abs((valorReal - valorAproximado) / valorReal);
+        System.out.printf("Error Relativo: %.4f%n", errorRelativo);
+    }
+}
 
