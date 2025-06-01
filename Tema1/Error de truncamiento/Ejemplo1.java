39c30a0dab9606e77a2a7e552000ae7f0632f7fe
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-06-01 13:03:44 -0600

    Update Ejemplo1.java

--- Tema1/Error de truncamiento/Ejemplo1.java
+++ Tema1/Error de truncamiento/Ejemplo1.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+
+public class TruncamientoEjemplo1 {
+
+    public static ArrayList<Double> calcularErrorTruncamiento(double[] valores, int decimales) {
+        ArrayList<Double> errores = new ArrayList<>();
+        double factor = Math.pow(10, decimales);
+
+        for (double valor : valores) {
+            // Truncamiento simple usando casting a int tras multiplicar
+            double truncado = ((int)(valor * factor)) / factor;
+            double error = Math.abs(valor - truncado);
+            errores.add(error);
+        }
+        return errores;
+    }
+
+    public static void main(String[] args) {
+        double[] valores = {3.14159, 2.71828, 1.61803};
+        int decimales = 2;
+        System.out.println("Ejemplo 1 - Errores de Truncamiento: " + calcularErrorTruncamiento(valores, decimales));
+    }
+}
 
