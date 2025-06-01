4f963223b762256f587da6f609712213da8b833d
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-06-01 13:00:29 -0600

    Update Ejemplo2.java

--- Tema1/Error Relativo/Ejemplo2.java
+++ Tema1/Error Relativo/Ejemplo2.java
@@ -1,22 +1,10 @@
-import java.util.ArrayList;
+public class Main {
 
-public class Ejemplo2 {
-
-    public static ArrayList<Double> calcularErrorRedondeo(double[] valores, int decimales) {
-        ArrayList<Double> errores = new ArrayList<>();
-        double factor = Math.pow(10, decimales);
-
-        for (double valor : valores) {
-            double valorRedondeado = Math.round(valor * factor) / factor;
-            double error = Math.abs(valor - valorRedondeado);
-            errores.add(error);
-        }
-        return errores;
+    public static double calcularErrorRelativo(double real, double aprox) {
+        return Math.abs((real - aprox) / real);
     }
 
     public static void main(String[] args) {
-        double[] valores = {9.87654, 7.12345, 6.99999};
-        int decimales = 3;
-        System.out.println("Ejemplo 2 - Errores de Redondeo: " + calcularErrorRedondeo(valores, decimales));
+        System.out.printf("Error Relativo: %.4f%n", calcularErrorRelativo(12.3, 11.9));
     }
 }
