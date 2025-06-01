eb8af70a42c5c52ee70222528e2dcf89385f79eb
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-06-01 17:30:36 -0600

    Update Ejemplo5.java

---
+++ Tema5/Metodo de Correlacion/Ejemplo5.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] x = {1, 2, 3};
+        double[] y = {2, 4, 6};
+        int n = x.length;
+        double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+        for (int i = 0; i < n; i++) {
+            sumX += x[i];
+            sumY += y[i];
+            sumXY += x[i] * y[i];
+            sumX2 += x[i] * x[i]; // CORRECTO
+        }
+        double b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+        double a = (sumY - b * sumX) / n;
+
+        System.out.printf("Problema 5: y = %.3f + %.3fx\n", a, b);
+    }
+}
