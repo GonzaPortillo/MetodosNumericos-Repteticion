6c7e717fd8c8ac9615d9d7682e80800b8393cd0f
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-06-01 13:01:10 -0600

    Update Ejemplo1.java

--- Tema1/Error de redondeo/Ejemplo1.java
+++ Tema1/Error de redondeo/Ejemplo1.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+public class Ejemplo1 {
+
+    public static ArrayList<Double> calcularErrorRedondeo(double[] valores, int decimales) {
+        ArrayList<Double> errores = new ArrayList<>();
+        double factor = Math.pow(10, decimales);
+
+        for (double valor : valores) {
+            double valorRedondeado = Math.round(valor * factor) / factor;
+            double error = Math.abs(valor - valorRedondeado);
+            errores.add(error);
+        }
+        return errores;
+    }
+
+    public static void main(String[] args) {
+        double[] valores = {3.14159, 2.71828, 1.61803};
+        int decimales = 2;
+        System.out.println("Ejemplo 1 - Errores de Redondeo: " + calcularErrorRedondeo(valores, decimales));
+    }
+}
