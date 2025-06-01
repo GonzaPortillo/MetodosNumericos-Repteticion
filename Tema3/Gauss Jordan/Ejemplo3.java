c438a91d00916db20e985c4eda93d04da42968a2
Gonza Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2025-06-01 14:08:51 -0600

    Update Ejemplo3.java

---
+++ Tema3/Gauss Jordan/Ejemplo3.java
@@ -0,0 +1,48 @@
+public class Problema_3 {
+
+    public static void main(String[] args) {
+        double[][] matriz = {
+            {2, -1, 1, 0},
+            {3, 2, -2, 0},
+            {1, -3, 1, 0}
+        };
+    
+        gaussJordan(matriz);
+    
+        System.out.println("Solución del sistema de ecuaciones:");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("x" + (i+1) + " = " + matriz[i][matriz[i].length-1]);
+        }
+    }
+
+    public static void gaussJordan(double[][] matriz) {
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+    
+        for (int i = 0; i < filas; i++) {
+            int maxFila = i;
+            for (int k = i + 1; k < filas; k++) {
+                if (Math.abs(matriz[k][i]) > Math.abs(matriz[maxFila][i])) {
+                    maxFila = k;
+                }
+            }
+            double[] temp = matriz[maxFila];
+            matriz[maxFila] = matriz[i];
+            matriz[i] = temp;
+        
+            double pivote = matriz[i][i];
+            for (int j = i; j < columnas; j++) {
+                matriz[i][j] /= pivote;
+            }
+        
+            for (int k = 0; k < filas; k++) {
+                if (k != i) {
+                    double factor = matriz[k][i];
+                    for (int j = i; j < columnas; j++) {
+                        matriz[k][j] -= factor * matriz[i][j];
+                    }
+                }
+            }
+        }
+    }
+}
